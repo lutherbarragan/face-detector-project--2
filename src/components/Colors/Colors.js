f49529b380212ec3605bec5f3bbd460278237351
Luther <luther0211@hotmail.com>
2019-07-15 17:57:10 -0500

    Updated Color component to smart component / updated Color component's methods

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
@@ -8,41 +8,48 @@ import { Link } from "react-router-dom";
 
 import './Colors.css';
 
-const Colors = () => {
-
-    let url = '';
-
-    const getInputValue = e => {
-        url = e.target.value;
-    }
-
-    const submitInputValue = e => {
-        console.log(url)
+class Colors extends Component {
+    state = {
+        inputValue: ''
     }
 
 
-    return (
-        <div className="colors">
-            <h1 className="colors__heading text-light bg-dark py-4 shadow">COLORS</h1>
-            <Link to="/">
-                <Button className="colors__back-btn mt-2">HOME</Button>
-            </Link>
-            <InputGroup className="my-3 mx-auto w-50" size="lg">
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="basic-addon1"><i className="fas fa-link"></i></InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                    placeholder="Please Enter an Image URL"
-                    aria-label="Image URL"
-                    aria-describedby="basic-addon1"
-                    onChange={(e) => getInputValue(e)}
-                />
-                <InputGroup.Append>
-                    <Button onClick={(e) => submitInputValue(e)}>SUBMIT</Button>
-                </InputGroup.Append>
-            </InputGroup>
-        </div>
-    )
+    getInputValue = e => {
+        this.setState( {inputValue: e.target.value} )
+    }
+
+    submitInputValue = e => {
+        console.log(this.state.inputValue);
+
+        
+        
+    }
+
+
+    render() {
+        return (
+            <div className="colors">
+                <h1 className="colors__heading text-light bg-dark py-4 shadow">COLORS</h1>
+                <Link to="/">
+                    <Button className="colors__back-btn mt-2">HOME</Button>
+                </Link>
+                <InputGroup className="my-3 mx-auto w-50" size="lg">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1"><i className="fas fa-link"></i></InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="Please Enter an Image URL"
+                        aria-label="Image URL"
+                        aria-describedby="basic-addon1"
+                        onChange={(e) => this.getInputValue(e)}
+                    />
+                    <InputGroup.Append>
+                        <Button className="btn-dark" onClick={(e) => this.submitInputValue(e)}>SUBMIT</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            </div>
+        )
+    }
 }
 
 export default Colors;
