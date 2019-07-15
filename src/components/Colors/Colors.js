4816d18328685aa9bdcd15208aedc1023ef37666
Luther <luther0211@hotmail.com>
2019-07-15 18:06:14 -0500

    Updated Color component

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -10,19 +10,22 @@ import './Colors.css';
 
 class Colors extends Component {
     state = {
-        inputValue: ''
+        inputValue: '',
+        submitValue: ''
     }
 
 
     getInputValue = e => {
+        e.preventDefault()
+
         this.setState( {inputValue: e.target.value} )
     }
 
     submitInputValue = e => {
-        console.log(this.state.inputValue);
+        e.preventDefault()
 
-        
-        
+        this.setState( {submitValue: this.state.inputValue} )
+        console.log(this.state.inputValue);       
     }
 
 
@@ -30,23 +33,30 @@ class Colors extends Component {
         return (
             <div className="colors">
                 <h1 className="colors__heading text-light bg-dark py-4 shadow">COLORS</h1>
+                
                 <Link to="/">
                     <Button className="colors__back-btn mt-2">HOME</Button>
                 </Link>
-                <InputGroup className="my-3 mx-auto w-50" size="lg">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1"><i className="fas fa-link"></i></InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="Please Enter an Image URL"
-                        aria-label="Image URL"
-                        aria-describedby="basic-addon1"
-                        onChange={(e) => this.getInputValue(e)}
-                    />
-                    <InputGroup.Append>
-                        <Button className="btn-dark" onClick={(e) => this.submitInputValue(e)}>SUBMIT</Button>
-                    </InputGroup.Append>
-                </InputGroup>
+                
+                <form onSubmit={(e) => this.submitInputValue(e)}>
+                    <InputGroup className="my-3 mx-auto w-50" size="lg">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="basic-addon1"><i className="fas fa-link"></i></InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            placeholder="Please Enter an Image URL"
+                            aria-label="Image URL"
+                            aria-describedby="basic-addon1"
+                            onChange={(e) => this.getInputValue(e)}
+                            />
+                        <InputGroup.Append>
+                            <Button className="btn-dark" type="submit">SUBMIT</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </form>
+
+
+
             </div>
         )
     }
