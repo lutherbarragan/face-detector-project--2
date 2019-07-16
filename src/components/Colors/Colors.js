d8c733d17f2329bffde0105368ef80781e94f829
Luther <luther0211@hotmail.com>
2019-07-16 11:42:44 -0500

    Colors component now renders the results

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -38,7 +38,10 @@ class Colors extends Component {
         this.setState( {submitValue: this.state.inputValue} )
 
         clarifaiApp.models.predict("eeed0b6733a644cea07cf4c60f87ebb7", this.state.inputValue)
-            .then( res => {console.log(res.outputs[0].data.colors)})
+            .then( res => {
+                console.log(res.outputs[0].data.colors)
+                this.setState( {resultColors: res.outputs[0].data.colors} )
+            })
             .catch(err => console.log(err))
     
         
@@ -49,15 +52,26 @@ class Colors extends Component {
 
         let imageResult = '';
 
+        let height = `${100 / this.state.resultColors.length}%`
+        let colors = this.state.resultColors.map((c, i) => {
+            return <p className="colors__color m-0 px-2" key={i} style={{backgroundColor: c.w3c.hex, height: height }} >
+                <span>{c.w3c.hex}</span>
+                <span>{c.w3c.name}</span>
+                <span>{Math.round(c.value * 100)}%</span>
+            </p>
+        })
+
         if(this.state.submitValue) {
             imageResult = (
-                <Container className="bg-dark">
+                <Container className="colors__result-container rounded">
                     <Row>
-                        <Col xs={12} md={6}>
+                        <Col className="bg-dark p-2" xs={12} md={6}>
                             <Image fluid  src={this.state.submitValue} alt="Result" />
                         </Col>
-                        <Col xs={12} md={6}>
-                            RESULT DATA
+                        <Col className="bg-dark pt-0 pb-2 px-2 py-md-2 pl-md-0 pr-md-2" xs={12} md={6}>
+                            <div className="h-100">
+                                {colors}
+                            </div>
                         </Col>
                     </Row>
                 </Container>
@@ -73,12 +87,12 @@ class Colors extends Component {
                 </Link>
                 
                 <form onSubmit={(e) => this.submitInputValue(e)}>
-                    <InputGroup className="my-3 mx-auto w-50" size="lg">
+                    <InputGroup className="colors__input my-3 mx-auto" size="md">
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-addon1"><i className="fas fa-link"></i></InputGroup.Text>
                         </InputGroup.Prepend>
                         <FormControl
-                            placeholder="Please Enter an Image URL"
+                            placeholder="Enter an Image URL"
                             aria-label="Image URL"
                             aria-describedby="basic-addon1"
                             onChange={(e) => this.getInputValue(e)}
