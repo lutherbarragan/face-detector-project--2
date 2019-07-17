f4d6498737343ab952d60383324f2e47a1452718
Luther <luther0211@hotmail.com>
2019-07-16 20:00:28 -0500

    Demographics component now renders faceBoxes and activeFaceBox data

--- src/components/Demographics/Demographics.js
+++ src/components/Demographics/Demographics.js
@@ -5,6 +5,9 @@ import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,6 +15,8 @@ import Col from 'react-bootstrap/Col';
 import { Link } from "react-router-dom";
 
 import './Demographics.css';
+
+
 
 const clarifaiApp = new Clarifai.App({
     apiKey: '9a8585cb625d4219b1acedf7c71116f4'
@@ -28,7 +33,25 @@ class Demographics extends Component {
 
 
     getActiveBoxData = e => {
-        console.log(e.target.id)
+        const id = e.target.id
+        const data = this.state.resultData.find(el => el.id === id);
+        
+        
+        const age = data.data.face.age_appearance.concepts.splice(0, 5).map(c => {
+            return { name: c.name, value: Math.round(c.value * 100) }
+        })
+
+        const gender = data.data.face.gender_appearance.concepts.map(c => {
+            return { name: c.name, value: Math.round(c.value * 100) }
+        })
+        
+        const multicultural = data.data.face.multicultural_appearance.concepts.map(c => {
+            return { name: c.name, value: Math.round(c.value * 100) }
+        })
+
+        const activeBox = {age, gender, multicultural}
+           
+        this.setState( {activeBox: activeBox} )
     }
 
     getInputValue = e => {
@@ -64,6 +87,10 @@ class Demographics extends Component {
         let imageResult = '';
         let faceBoxes = [];
 
+        let age = <p className="font-weight-bold p-0 m-0">CLICK ON A FACE</p>;
+        let gender = <p className="font-weight-bold p-0 m-0">CLICK ON A FACE</p>;
+        let multicultural = <p className="font-weight-bold p-0 m-0">CLICK ON A FACE</p>;
+
 
         
         if(this.state.resultBoxes) {
@@ -77,29 +104,78 @@ class Demographics extends Component {
                 return <div className="demographics__face-box border-primary" id={box.id} key={box.id} style={boxSize} onClick={e => this.getActiveBoxData(e)}></div>
             })
         }
-        
-        
+         
+        if(this.state.activeBox.age) {
+            console.log(this.state.activeBox)
+            age = this.state.activeBox.age.map((c, i) => {
+                if(i % 2 === 0) {
+                    return <p className="d-flex justify-content-between py-1 px-2 m-0 text-light even" key={i}><span>{c.name} years old</span> <span>{c.value}%</span></p>
+                } else {
+                    return <p className="d-flex justify-content-between py-1 px-2 m-0 text-light odd" key={i}><span>{c.name} years old</span> <span>{c.value}%</span></p>
+                }
+            })
+
+            gender = this.state.activeBox.gender.map((c, i) => {
+                if(i % 2 === 0) {
+                    return <p className="d-flex justify-content-between py-1 px-2 m-0 text-light even" key={i}><span>{c.name}</span> <span>{c.value}%</span></p>
+                } else {
+                    return <p className="d-flex justify-content-between py-1 px-2 m-0 text-light odd" key={i}><span>{c.name}</span> <span>{c.value}%</span></p>
+                }
+            })
+
+            multicultural = this.state.activeBox.multicultural.map((c, i) => {
+                if(i % 2 === 0) {
+                    return <p className="d-flex justify-content-between py-1 px-2 m-0 text-light even" key={i}><span>{c.name}</span> <span>{c.value}%</span></p>
+                } else {
+                    return <p className="d-flex justify-content-between py-1 px-2 m-0 text-light odd" key={i}><span>{c.name}</span> <span>{c.value}%</span></p>
+                }
+            })
+        }
+         
         if(this.state.submitValue) {
             imageResult = (
                 <Container className="demographics__result-container rounded">
                     <Row>
-                        <Col className="bg-dark p-2" xs={12} md={6}>
-                            <div className="position-relative">
+                        <Col className="bg-dark p-3 p-lg-5" xs={12} lg={8}>
+                            <div className="demographics__image-box position-relative">
                                 <Image fluid  src={this.state.submitValue} alt="Result" />
                                 {faceBoxes}
                             </div>
                         </Col>
-                        <Col className="bg-dark pt-0 pb-2 px-2 py-md-2 pl-md-0 pr-md-2" xs={12} md={6}>
+                        <Col className="bg-light p-2" xs={12} lg={4}>
                             <div className="h-100">
-                                
+                            <Accordion>
+                                <Card>
+                                    <Accordion.Toggle className="demographics__accordion-heading bg-dark text-light font-weight-bold" as={Card.Header} eventKey="0">
+                                        AGE
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="0">
+                                        <Card.Body className="p-0">{age}</Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                <Card>
+                                    <Accordion.Toggle className="demographics__accordion-heading bg-dark text-light font-weight-bold" as={Card.Header} eventKey="1">
+                                        GENDER
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="1">
+                                        <Card.Body className="p-0">{gender}</Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                <Card>
+                                    <Accordion.Toggle className="demographics__accordion-heading bg-dark text-light font-weight-bold" as={Card.Header} eventKey="2">
+                                        MULTICULTURAL APPEARANCE
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="2">
+                                        <Card.Body className="p-0">{multicultural}</Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                            </Accordion>
                             </div>
                         </Col>
                     </Row>
                 </Container>
             )
         }
-
-
 
 
         return (
